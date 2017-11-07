61296abfb9d7d8cb70b6becd9b61947d47f18f71
anne belakonis <anne.belakonis@libertymutual.com>
2017-11-07 13:58:02 -0500

    Updated HTML and app.event.js
    --- added radio button, drop down for options and forms for create survey
    ---added function to app.event.js to hide and show radio buttons and
    ---questions when drop down is selected

--- assets/scripts/application/app_events.js
+++ assets/scripts/application/app_events.js
@@ -1,5 +1,5 @@
 'use strict'
-// const getFormFields = require('../../../lib/get-form-fields')
+const getFormFields = require('../../../lib/get-form-fields')
 const appUi = require('./app_ui')
 const appApi = require('./app_api')
 
@@ -13,9 +13,32 @@ const ontoggleSurveyList = function (event) {
   appUi.toggleSurveyList()
 }
 
+// const displayQuestions = function () {
+$('select').change(function () {
+  $('select option:selected').each(function () {
+    if ($(this).val() === '1') {
+      $('#question1').removeClass('hidden')
+      $('#question2, #question3').addClass('hidden')
+    } else if ($(this).val() === '2') {
+      $('#question1, #question2').removeClass('hidden')
+      $('#question3').addClass('hidden')
+    } else if ($(this).val() === '3') {
+      $('#question1, #question2, #question3').removeClass('hidden')
+    }
+  })
+})
+
 const onGetSurveys = function (data) {
   // event.preventDefault()
   appApi.index()
+    .then(appUi.onGetSurveysSuccess)
+    .catch(appUi.onError)
+}
+
+const onCreateSurveys = function (event) {
+  event.preventDefault()
+  const data = getFormFields(this)
+  appApi.create(data)
     .then(appUi.onGetSurveysSuccess)
     .catch(appUi.onError)
 }
@@ -28,5 +51,6 @@ const addHandlers = function () {
 
 module.exports = {
   addHandlers,
-  onGetSurveys
+  onGetSurveys,
+  onCreateSurveys
 }
